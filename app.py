db094de3d7c35e22f3e29ba9c8fb4108f0243997
Rohit Chandra <rohitv.chandra@gmail.com>
2023-02-07 23:41:31 -0800

    fixed the width of the image size

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from diffusers import StableDiffusionPipeline
 
 # create the app
 app = tk.Tk()
-app.geometry("532*622")
+app.geometry("532*632")
 app.title("Stable Diffusion App")
 ctk.set_appearance_mode("dark")
 
@@ -28,16 +28,27 @@ lmain.place(x = 10, y = 110)
 model_id = "CompVis/stable-diffusion-v1-4"
 
 # create the pipeline
-pipe = StableDiffusionPipeline.from_pretrained(model_id, revision = "fp16", torch_dtypes = torch.float16, use_auth_token = auth_token)
+pipe = StableDiffusionPipeline.from_pretrained(model_id, revision = "fp16", torch_dtype = torch.float16, use_auth_token = auth_token)
 
 # send this pipe to gPU
 device = "cuda"
+# here, model is loaded
 pipe.to(device)
 
 
 
 def generate():
-    pass
+    # creates an image
+    with autocast(device):
+        # pass the prompt. Guidnace scale is how close the image is to the prompt
+        image = pipe(prompt.get(), gudiance_scale = 8.5)["sample"][0]
+    # save the img
+    image.save("generatedimage.png")  
+    # create the image
+    img = ImageTk.PhotoImage(image)
+    
+    
+    lmain.configure(image = img)
 
 # button to generate image
 trigger = ctk.CTkButton(text = "Generate", height = 40, width = 512, text_font = ("Arial", 20), text_color = "white", fg_color = "blue",  command=generate)
